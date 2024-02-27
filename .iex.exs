0e91d9889a30a1008b6350cbed6e557709d6cb51
Rodrigo Zampieri Castilho <rodrigo.zampieri@gmail.com>
2024-02-27 10:45:00 -0300

    feat: create collection config command

--- .iex.exs
+++ .iex.exs
@@ -2,8 +2,3 @@ alias SolrCli.{
   Controller,
     HttpClient
 }
-
-client = HttpClient.new(base_url: "http://dev.k8s.internal.geofusion/solr")
-
-%{body: %{"cluster" => %{"collections" => collections, "aliases" => aliases}}} =
-  Tesla.get!(client, "/admin/collections?action=CLUSTERSTATUS")
